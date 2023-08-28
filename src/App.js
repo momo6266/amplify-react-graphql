87a142b16763a3f21b7f98307c0858c7377fc59f
momo6266 <zhijing914@gmail.com>
2023-08-28 22:15:40 +0800

    Testing

--- src/App.js
+++ src/App.js
@@ -1,135 +1,99 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import "@aws-amplify/ui-react/styles.css";
-import { API, Storage } from 'aws-amplify';
+import React, { useState } from 'react'
+import './App.css'
+import Amplify, { Storage } from 'aws-amplify'
 import {
-  Button,
-  Flex,
-  Heading,
-  Image,
-  Text,
-  TextField,
-  View,
-  withAuthenticator,
-} from '@aws-amplify/ui-react';
-import { listNotes } from "./graphql/queries";
-import {
-  createNote as createNoteMutation,
-  deleteNote as deleteNoteMutation,
-} from "./graphql/mutations";
+  AmplifyAuthenticator,
+  AmplifySignOut,
+  AmplifySignIn,
+  AmplifySignUp,
+} from '@aws-amplify/ui-react'
+import { MdSend /* MdList */ } from 'react-icons/md'
+import awsConfig from './aws-exports'
+Amplify.configure(awsConfig)
 
-const App = ({ signOut }) => {
-  const [notes, setNotes] = useState([]);
+const App = () => {
+  const [name, setName] = useState('')
+  const [file, setFile] = useState('')
+  const [response, setResponse] = useState('')
 
-  useEffect(() => {
-    fetchNotes();
-  }, []);
+  const onChange = (e) => {
+    e.preventDefault()
+    if (e.target.files[0] !== null) {
+      setFile(e.target.files[0])
+      setName(e.target.files[0].name)
+    }
+  }
 
-  async function fetchNotes() {
-    const apiData = await API.graphql({ query: listNotes });
-    const notesFromAPI = apiData.data.listNotes.items;
-    await Promise.all(
-      notesFromAPI.map(async (note) => {
-        if (note.image) {
-          const url = await Storage.get(note.name);
-          note.image = url;
-        }
-        return note;
+  const onSubmit = (e) => {
+    e.preventDefault()
+    if (file) {
+      Storage.put(name, file, {
+        /* level: 'protected', */
+        contentType: file.type,
       })
-    );
-    setNotes(notesFromAPI);
-  }
-
-  async function createNote(event) {
-    event.preventDefault();
-    const form = new FormData(event.target);
-    const image = form.get("image");
-    const data = {
-      name: form.get("name"),
-      description: form.get("description"),
-      image: image.name,
-    };
-    if (!!data.image) await Storage.put(data.name, image);
-    await API.graphql({
-      query: createNoteMutation,
-      variables: { input: data },
-    });
-    fetchNotes();
-    event.target.reset();
-  }
-  
-
-  async function deleteNote({ id, name }) {
-    const newNotes = notes.filter((note) => note.id !== id);
-    setNotes(newNotes);
-    await Storage.remove(name);
-    await API.graphql({
-      query: deleteNoteMutation,
-      variables: { input: { id } },
-    });
+        .then((result) => {
+          console.log(result)
+          setResponse(`Success uploading file: ${name}!`)
+        })
+        .then(() => {
+          document.getElementById('file-input').value = null
+          setFile(null)
+        })
+        .catch((err) => {
+          console.log(err)
+          setResponse(`Can't upload file: ${err}`)
+        })
+    } else {
+      setResponse(`Files needed!`)
+    }
   }
 
   return (
-    <View className="App">
-      <Heading level={1}>My Notes App</Heading>
-      <View as="form" margin="3rem 0" onSubmit={createNote}>
-        <Flex direction="row" justifyContent="center">
-          <TextField
-            name="name"
-            placeholder="Note Name"
-            label="Note Name"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <TextField
-            name="description"
-            placeholder="Note Description"
-            label="Note Description"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <Button type="submit" variation="primary">
-            Create Note
-          </Button>
-        </Flex>
-      </View>
-      <View
-        name="image"
-        as="input"
-        type="file"
-        style={{ alignSelf: "end" }}
+    <AmplifyAuthenticator>
+      <AmplifySignIn
+        headerText='AWS TCS Team, Sign-In with Your E-Mail Address'
+        slot='sign-in'
       />
-      <Heading level={2}>Current Notes</Heading>
-      <View margin="3rem 0">
-      {notes.map((note) => (
-  <Flex
-    key={note.id || note.name}
-    direction="row"
-    justifyContent="center"
-    alignItems="center"
-  >
-    <Text as="strong" fontWeight={700}>
-      {note.name}
-    </Text>
-    <Text as="span">{note.description}</Text>
-    {note.image && (
-      <Image
-        src={note.image}
-        alt={`visual aid for ${notes.name}`}
-        style={{ width: 400 }}
+      <AmplifySignUp
+        headerText='AWS TCS Team, Sign-Up with Your Valid E-Mail Address'
+        slot='sign-up'
       />
-    )}
-    <Button variation="link" onClick={() => deleteNote(note)}>
-      Delete note
-    </Button>
-  </Flex>
-))}
-      </View>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
-  );
-};
+      <div className='header'>
+        <h2>
+          <a href='/'>TCS Team Video Uploader</a>
+        </h2>
+      </div>
+      <div className='video-uploader'>
+        <form onSubmit={(e) => onSubmit(e)}>
+          <p>
+            <label className='select-label'>Select video: </label>
+          </p>
+          <p>
+            <input
+              className='video-input'
+              type='file'
+              id='file-input'
+              accept='image/*, video/*'
+              onChange={(e) => onChange(e)}
+            />
+          </p>
+          <button type='submit' className='btn'>
+            Submit <MdSend className='btn-icon' />
+          </button>
+        </form>
+      </div>
 
-export default withAuthenticator(App);
+      {response && (
+        <div id='upload-status' className='upload-status'>
+          {response}
+        </div>
+      )}
+
+      <div className='sign-out'>
+        <AmplifySignOut />
+      </div>
+    </AmplifyAuthenticator>
+  )
+}
+
+export default App
